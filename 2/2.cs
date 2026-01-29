ef10ed984116d9229b9c9889499a3f2a6de14c23
maxlipas <maxlipskiy2009@gmail.com>
2026-01-30 01:45:07 +0300

    G

---
+++ 2/2.cs
@@ -0,0 +1,128 @@
+﻿using System;
+Console.WriteLine("=== Работа с рациональными дробями ===");
+
+// Ввод первой дроби
+Console.Write("Введите числитель первой дроби: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите знаменатель первой дроби: ");
+int b = int.Parse(Console.ReadLine()!);
+
+// Ввод второй дроби
+Console.Write("Введите числитель второй дроби: ");
+int c = int.Parse(Console.ReadLine()!);
+Console.Write("Введите знаменатель второй дроби: ");
+int d = int.Parse(Console.ReadLine()!);
+
+Rational plus = new Rational(a, b, c, d);
+string sum = plus.Sum();
+Console.WriteLine($"{a}/{b} + {c}/{d} = {sum}");
+string sub = plus.Sub();
+Console.WriteLine($"{a}/{b} - {c}/{d} = {sub}");
+string mul = plus.Mul();
+Console.WriteLine($"{a}/{b} * {c}/{d} = {mul}");
+string div = plus.Div();
+Console.WriteLine($"{a}/{b} / {c}/{d} = {div}");
+string srav = plus.Srav();
+Console.WriteLine(srav);
+
+abstract class Drobi
+{
+    public abstract string Sum();
+    public abstract string Sub();
+    public abstract string Mul();
+    public abstract string Div();
+    public abstract string Srav();
+}
+
+class Rational : Drobi
+{
+    private double a; private double b; private double c; private double d;
+    public double A
+    {
+        get { return a; }
+        set { a = value; }
+    }
+    public double B
+    {
+        get { return b; }
+        set { b = value; }
+    }
+    public double C
+    {
+        get { return c; }
+        set { c = value; }
+    }
+    public double D
+    {
+        get { return d; }
+        set { d = value; }
+    }
+    public Rational(double A, double B, double C, double D)
+    {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+    }
+
+    public override string Sum()
+    {
+        double numerator = A * D + C * B;
+        double denominator = B * D;
+        return Reduce(numerator, denominator);
+    }
+    public override string Sub()
+    {
+        double numerator = A * D - C * B;
+        double denominator = B * D;
+        return Reduce(numerator, denominator);
+    }
+    public override string Mul()
+    {
+        double numerator = A * C;
+        double denominator = B * D;
+        return Reduce(numerator, denominator);
+    }
+    public override string Div()
+    {
+        double numerator = A * D ;
+        double denominator = B * C;
+        return Reduce(numerator, denominator);
+    }
+    public override string Srav()
+    {
+        double drob1 = (double)A / B;
+        double drob2 = (double)C / D;
+
+        if (drob1 < drob2)
+        {
+            return $"{A}/{B} < {C}/{D}";
+        }
+        else if (drob2 < drob1)
+        {
+            return $"{A}/{B} > {C}/{D}";
+        }
+        else
+        {
+            return $"{A}/{B} = {C}/{D}";
+        }
+    }
+    private string Reduce(double numerator, double denominator)
+    {
+        double gcd = GCD((int)Math.Abs(numerator), (int)Math.Abs(denominator));
+        numerator /= gcd;
+        denominator /= gcd;
+        return $"{numerator}/{denominator}";
+    }
+
+    private int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
---
+++ ConsoleApp1/1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+    Console.Write("Введите год: ");
+    double year = double.Parse(Console.ReadLine()!);
+    Console.Write("Введите месяц: ");
+    double month = double.Parse(Console.ReadLine()!);
+    Console.Write("Введите день: ");
+    double day = double.Parse(Console.ReadLine()!);
+
+    Date date = new Date(year, month, day);
+    date.Increase();
+    Console.WriteLine(date);
+
+        Console.Write("Введите часы: ");
+        double hours = double.Parse(Console.ReadLine()!);
+        Console.Write("Введите минуты: ");
+        double minutes = double.Parse(Console.ReadLine()!);
+        Console.Write("Введите секунды: ");
+        double seconds = double.Parse(Console.ReadLine()!);
+
+        Time time = new Time(hours, minutes, seconds);
+        time.Increase();
+        Console.WriteLine(time);
+abstract class Triad
+{
+    public abstract double Increase();
+}
+
+class Date : Triad
+{
+    private double year;
+    private double month;
+    private double day;
+    public double Year
+    {
+        get { return year; }
+        set { if (value > 0) year = value; }
+    }
+    public double Month
+    {
+        get { return month; }
+        set { if (value > 0 && value <= 12) month = value; }
+    }
+    public double Day
+    {
+        get { return day; }
+        set { if (value > 0 && value <= 31) day = value; }
+    }
+    public Date(double year, double month, double day)
+    {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+    public override double Increase()
+    {
+        double oldSum = year + month + day;
+
+        day += 1;
+        month += 1;
+        year += 1;
+        if (day > 31) { day -= 31; month++; }
+        if (month > 12){ month -= 12; year++; }
+
+        double newSum = year + month + day;
+        return 404;
+    }
+    public override string ToString()
+    {
+        return $"Дата увеличенная на 1: {year}.{month}.{day}";
+    }
+}
+
+class Time : Triad
+{
+    private double hours;
+    private double minutes;
+    private double seconds;
+
+    public double Hours
+    {
+        get { return hours; }
+        set { if (value >= 0 && value < 24) hours = value; }
+    }
+
+    public double Minutes
+    {
+        get { return minutes; }
+        set { if (value >= 0 && value < 60) minutes = value; }
+    }
+
+    public double Seconds
+    {
+        get { return seconds; }
+        set { if (value >= 0 && value < 60) seconds = value; }
+    }
+
+    public Time(double hours, double minutes, double seconds)
+    {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public override double Increase()
+    {
+        double oldTotal = hours * 3600 + minutes * 60 + seconds;
+        seconds += 1;
+        minutes += 1;
+        hours += 1;
+        if (seconds >= 60)
+        {
+            seconds -= 60;
+            minutes++;
+        }
+        if (minutes >= 60)
+        {
+            minutes -= 60;
+            hours++;
+        }
+        if (hours >= 24)
+        {
+            hours -= 24;
+        }
+        double newTotal = hours * 3600 + minutes * 60 + seconds;
+        return 404;
+    }
+    public override string ToString()
+    {
+        return $"Время увеличенное на 1: {hours}:{minutes}:{seconds}";
+    }
+}
